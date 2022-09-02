aad82a4d98dc5197ce4e2f98af22b09231d27276
prashantkum54 <112720186+prashantkum54@users.noreply.github.com>
2022-09-02 23:13:18 +0530

    Add files via upload

---
+++ aa1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class area
+{
+public static void main(String[] args)
+{
+Scanner sc = new Scanner(System.in);
+double r,area;
+System.out.println("Enter Radius of the Circle.");
+r = sc.nextDouble();
+area = 3.14 * r * r;
+System.out.println("Area of the Circle is "+area);
+}
+}
